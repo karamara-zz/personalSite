c33b323653585aa961076069601b349b01267d01
Sung Kang <sungjk@msn.com>
2016-03-13 23:26:59 -0700

    made the nav bar active

--- static/js/app.js
+++ static/js/app.js
@@ -8,3 +8,24 @@ var app = angular.module('app', ['ngRoute'])
 		redirectTo: '/'
 	})
 })
+.directive('scrollOnClick', function() {
+  return {
+    restrict: 'A',
+    link: function(scope, $elm, attrs) {
+      var idToScroll = attrs.href;
+      $elm.on('click', function() {
+        var $target;
+        if (idToScroll) {
+          $target = $(idToScroll);
+        } else {
+          $target = $elm;
+        }
+        $("body").animate({scrollTop: $target.offset().top}, "slow");
+      });
+    }
+  }
+});
+
+// .run(['$anchorScroll', function($anchorScroll){
+// 	$anchorScroll.yOffset = 50;
+// }])
